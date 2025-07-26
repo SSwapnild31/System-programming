bc63ffed5f3fd17392fbe4e7f26b3d02cb91d1ab
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-07-26 22:50:54 +0530

    26-07-2025 Sat

---
+++ file_mgmt/ex5_print_file_is_link_or_not.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<sys/stat.h>
+
+void main(int argc,char **argv)
+{
+	if(argc!=3)
+	{
+		printf("Usage : ./a.out fname1 fname2\n ");
+		return ;
+	}
+	
+	struct stat v,v1;
+	
+	if(stat(argv[1],&v)<0)
+	{
+		perror("stat");
+		return ;
+	}
+
+	if(stat(argv[2],&v1)<0)
+	{
+		perror("stat");
+		return ;
+	}
+	
+	if(v.st_ino == v1.st_ino)
+	{
+		lstat(argv[1],&v);
+		lstat(argv[2],&v1);
+		if(v.st_ino == v1.st_ino)
+			printf("Hard link\n");
+		else
+			printf("Soft link\n");
+	}
+	else
+		printf("No link..!\n");
+}
