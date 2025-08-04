b87d6197b45caaedb06b1bd4aff82aa8fb08380c
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-08-04 14:51:11 +0530

    04-08-2025 Mon

---
+++ inter_process_commn/shared_memory/ex2_receive_data.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/shm.h>
+
+int main()
+{
+	int id = shmget(4,50,IPC_CREAT|0664);
+	if(id<0)
+	{
+		perror("shmget");
+		return 0;
+	}
+	printf("id : %d\n",id);
+	
+	char *p;
+	
+	p = shmat(id,0,0);
+	perror("shmat");
+	
+	printf("Data : %s\n",p);
+	
+	shmdt(p);
+}
