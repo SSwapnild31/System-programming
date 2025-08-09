6d1bd9693096087f42c28356a5ec9adb7373d584
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-08-09 23:42:50 +0530

    09-08-2025 Sat

---
+++ inter_process_commn/record_lock/ex1.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+int main()
+{
+	int fd = open("data",O_WRONLY|O_APPEND|O_CREAT,0664);
+	
+	if(fd<0)
+	{
+		perror("open");
+		return 0;
+	}
+	printf("fd : %d\n",fd);
+	
+	char ch;
+	
+	struct flock v;
+	
+	v.l_type = F_WRLCK;
+	v.l_whence = SEEK_SET;
+	v.l_start = 0;
+	v.l_len = 0;
+	
+	printf("before...\n");
+	fcntl(fd,F_SETLK,&v);
+	printf("After..\n");
+
+	for(ch='a';ch<='z';ch++)
+	{
+		write(fd,&ch,1);
+		sleep(1);
+	}
+	
+	printf("Done..\n");
+	v.l_type = F_UNLCK;
+	fcntl(fd,F_SETLK,&v);
+}
