0412559be366d963c772b4b967aae4fcaaf01c7f
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-08-16 21:48:21 +0530

    16-08-2025 Sat

---
+++ inter_process_commn/semaphore/w22.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/sem.h>
+
+int main()
+{	
+	int id = semget(4,5,IPC_CREAT|0664);
+	if(id<0)
+	{	
+		perror("semget");
+		return 0;
+	}
+	printf("id : %d\n",id);
+	
+	int fd = open("data",O_WRONLY|O_APPEND|O_CREAT,0664);
+	if(fd<0)
+	{
+		perror("open");
+		return 0;
+	}
+	printf("fd : %d\n",fd);
+	
+	struct sembuf v;
+	
+	v.sem_num = 3;
+	v.sem_op  = 0;
+	v.sem_flg = 0;
+	
+	for(char ch='A';ch<='Z';ch++)
+	{
+		semop(id,&v,1);
+		semctl(id,2,SETVAL,1);
+		write(fd,&ch,1);
+		semctl(id,2,SETVAL,0);
+		semctl(id,3,SETVAL,1);
+	}
+	printf("Done..\n");
+	return 0;
+}
