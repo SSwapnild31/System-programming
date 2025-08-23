77dc89e40739b3475551ddff5badd7e6cbd6be5b
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-08-23 23:04:25 +0530

    23-08-2025 sat

---
+++ thread_mgmt/ex3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<pthread.h>
+
+void *newt(void *p)
+{
+	int num = *(int*)p;
+	int i;
+	for(i=2;i<num;i++)
+	{
+		if(num%i==0)
+			break;
+	}
+	if(num==i)
+		printf("Number is prime\n");
+	else
+		printf("Number is not prime\n");
+}
+
+int main()
+{
+	int num;
+	printf("Enter number : ");
+	scanf("%d",&num);
+	
+	pthread_t t1;
+	pthread_create(&t1,0,newt,&num);
+	
+	pthread_exit(0);
+	
+	return 0;
+}
