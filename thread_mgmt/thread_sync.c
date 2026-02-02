e06868cdd745f2dcd523d481ab7fc86b189b7b9a
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-02-03 03:43:37 +0530

    03-02-2026 Tue
    Thread synchronization bug solved!

--- thread_mgmt/thread_sync.c
+++ thread_mgmt/thread_sync.c
@@ -1,49 +1,67 @@
 #include<stdio.h>
 #include<pthread.h>
-#include<unistd.h>
 
-pthread_mutex_t m1;
-pthread_t t1, t2;
+pthread_mutex_t mtx;
+pthread_cond_t cond;
+int turn = 0;
 
 void *thread1(void *arg){
 	char ch = 'a';
 
 	for(int i=0;i<26;i++){
-		pthread_mutex_lock(&m1);
+		pthread_mutex_lock(&mtx);
+		
+		while(turn != 0){
+			pthread_cond_wait(&cond, &mtx);
+		}
+		
 		printf("%c",ch++);
 		fflush(stdout);
-		pthread_mutex_unlock(&m1);
+		
+		turn = 1;
+		pthread_cond_signal(&cond);
+		pthread_mutex_unlock(&mtx);
 	}
-	pthread_exit(0);
+	return NULL;
 }
 
 void *thread2(void *arg){
 	char ch = 'A';
 	
 	for(int i=0;i<26;i++){
-		pthread_mutex_lock(&m1);
-		printf("%c",ch++);
-		fflush(stdout);
-		pthread_mutex_unlock(&m1);
-	}
-	pthread_exit(0);
-}
+		pthread_mutex_lock(&mtx);
 
-void mutex_init(void){
-	pthread_mutex_init(&m1, NULL);
+		while(turn != 1){
+			pthread_cond_wait(&cond, &mtx);
+		}
+
+		printf("%c ",ch++);
+		fflush(stdout);
+		
+		turn = 0;
+		pthread_cond_signal(&cond);
+		pthread_mutex_unlock(&mtx);
+	}
+	return NULL;
 }
 
 int main()
 {
-	mutex_init();
+	pthread_t t1, t2;
+
+	pthread_mutex_init(&mtx, NULL);
+	pthread_cond_init(&cond, NULL);
 
 	pthread_create(&t1, NULL, thread1, NULL);
 	pthread_create(&t2, NULL, thread2, NULL);
 	
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
+	
+	printf("\n");
 
-	pthread_mutex_destroy(&m1);
+	pthread_mutex_destroy(&mtx);
+	pthread_cond_destroy(&cond);
 	
 	return 0;
 }
